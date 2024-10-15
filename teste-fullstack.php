2dd961ca8bdd0de37ebd0c423105f6da8d97500a
Alexandre Aimbiré <alexvickers@gmail.com>
2024-10-15 14:27:22 -0300

    Testing API

--- teste-fullstack.php
+++ teste-fullstack.php
@@ -1,6 +1,16 @@
-/*
+<?php /*
 * Plugin Name: Teste Fullstack CNN
 * Plugin URI: https://github.com/alexvickers/teste-fullstack
 * Author: Alexandre Aimbiré
 * Author URI: https://github.com/alexvickers
 */
+
+function loteria_plugin() {
+
+    $fetch_api = wp_remote_get('https://loteriascaixa-api.herokuapp.com/api');
+
+    var_dump($fetch_api);
+
+}
+
+add_shortcode('teste_api', 'loteria_plugin');
